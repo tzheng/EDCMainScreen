ed42829896dc230540c777fff890029312e28da7
tzheng <tzheng@andrew.cmu.edu>
2013-07-08 15:19:53 -0400

    update the font family. in our design, we will use Helvetica as primary font. It is one of the most beautiful and popular font in web design.

--- js/template.js
+++ js/template.js
@@ -1,5 +1,29 @@
 
 
+/*Support dropdown menu*/
 $('.dropdown-toggle').dropdown();
 
+
+/* Display the toop tip*/
 $('.nav-tooltip').tooltip()
+
+
+
+$('.nav-popover').popover({html : true});
+$(document).ready(function(){
+  $('.nav-popover').popover({ 
+    html : true,
+    content: function() {
+      return $('#popover_content_wrapper').html();
+    }
+  });
+});
+
+$(document).ready(function(){
+  $('.danger').popover({ 
+    html : true,
+    content: function() {
+      return $('#popover_content_wrapper').html();
+    }
+  });
+});
