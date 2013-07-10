22735f5bcd689e7aeb5d924fd1f0ba323bb0308d
tzheng <tzheng@andrew.cmu.edu>
2013-07-09 20:43:39 -0400

    add sample data

--- js/edcmain.js
+++ js/edcmain.js
@@ -1,6 +1,6 @@
 $(document).ready(function()
 {
-
+   //fold and unfold div boxes
    $(".btn-minimize").click(function(){
    		var $target = $(this).parent().parent().parent().next('.box-content');
    		
@@ -9,4 +9,23 @@ $(document).ready(function()
 		
 		$target.slideToggle();
    });
+   
+   
+   	$('#add-regular').click(function(){
+
+		$.gritter.add({
+			// (string | mandatory) the heading of the notification
+			title: 'This is a regular notice!',
+			// (string | mandatory) the text inside the notification
+			text: 'This will fade out after a certain amount of time.',
+			// (string | optional) the image to display on the left
+			//image: 'img/avatar.jpg',
+			// (bool | optional) if you want it to fade out on its own or just sit there
+			sticky: false,
+			// (int | optional) the time you want it to be alive for before fading out
+			time: '5000'
+		});
+
+		return false;
+	});
 });
