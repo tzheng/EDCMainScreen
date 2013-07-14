8ea9cf7358d94f38e48f70f50e59af988c2c749e
tzheng <tzheng@andrew.cmu.edu>
2013-07-14 19:23:30 -0400

    Changes on the subject matrix of EDC Main screen. Create Layout of validation tool

--- js/template.js
+++ js/template.js
@@ -5,7 +5,7 @@ $('.dropdown-toggle').dropdown();
 
 
 /* Display the toop tip*/
-$('.nav-tooltip').tooltip()
+$('.nav-tooltip').tooltip();
 
 
 
@@ -34,7 +34,7 @@ $(document).ready(function(){
 
    //fold and unfold div boxes
    $(".btn-minimize").click(function(){
-   		var $target = $(this).parent().parent().parent().next('.box-content');
+   		var $target = $(this).parent().parent().next('.box-content');
    		
    		if($target.is(':visible')) $('i',$(this)).removeClass('icon-chevron-up').addClass('icon-chevron-down');
 		else 					   $('i',$(this)).removeClass('icon-chevron-down').addClass('icon-chevron-up');
